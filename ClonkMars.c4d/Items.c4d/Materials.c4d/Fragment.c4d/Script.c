44c4a6bdab706b8c5e2067f9f0fc02076383d364
JCaesar <doppelklick@hotmail.de>
2009-11-17 16:57:29 +0000

    ! Schrottgrafik ! Typos in Kapselactmap

--- ClonkMars.c4d/Items.c4d/Materials.c4d/Fragment.c4d/Script.c
+++ ClonkMars.c4d/Items.c4d/Materials.c4d/Fragment.c4d/Script.c
@@ -3,7 +3,8 @@
 #strict
 
 func Initialize() {
-  SetAction("%d",Random(3)+1);
+  SetAction(Format("g%d",Random(4)+1));
+  //SetGraphics (0, 0, 0, 0, GFXOV_MODE_Action, Format("g%d",Random(4)+1));
   return(1);
 }
 
