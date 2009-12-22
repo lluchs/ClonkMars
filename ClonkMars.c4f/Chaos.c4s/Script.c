050025ffdf154a0a66e721d9f479053d8583bacc
Nachtfalter <f.graier@gmx.de>
2009-12-22 13:11:36 +0000

    ! Picture in TEMP fixed + Chaos.c4s (unfertig)

---
+++ ClonkMars.c4f/Chaos.c4s/Script.c
@@ -0,0 +1,9 @@
+/*-- Eos --*/
+
+#strict 2
+
+func Initialize() {
+  SetSkyAdjust(RGBa(255,255,255,128),RGB(64,196,255));
+  SetGamma(RGB(50,0,0), RGB(140,100,100), RGB(255,220,220));
+  return(1);
+}
