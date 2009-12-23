5caf04d3bf1b698103eab04ecb4f632450338a6e
Luchs <Luchs@euirc.eu>
2009-12-23 16:42:02 +0000

    ! hängende Flagge nicht mehr vor anderen Objekten

--- ClonkMars.c4d/Items.c4d/Equipment.c4d/Sat.c4d/Script.c
+++ ClonkMars.c4d/Items.c4d/Equipment.c4d/Sat.c4d/Script.c
@@ -17,6 +17,7 @@ protected func Entrance(object pContainer) {
 	if(pContainer -> ~IsBase() && !FindObject2(Find_ID(GetID()), Find_Action("FlyBase"), Find_ActionTarget(pContainer))) {
 		SetAction("FlyBase", pContainer);
 		Sound("Sat_Beep");
+		SetCategory(C4D_Structure);
 		return 1;
 	}
 }
