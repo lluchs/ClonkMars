ea79f0c6e4fea94a3aac96cab0203149991c1dca
Luchs <Luchs@euirc.eu>
2009-12-23 14:52:22 +0000

    + Erforschen in der Basis

--- ClonkMars.c4d/Structures.c4d/Base.c4d/Script.c
+++ ClonkMars.c4d/Structures.c4d/Base.c4d/Script.c
@@ -10,6 +10,10 @@
 
 public func GetTemp() { return 800;}
 
+private func EnergyNeedOverlay() {
+	return 3; // Overlay 1 und 2: Türsteuerung
+}
+
 protected func Initialize() {
 	AddBuildingLight();
 	return _inherited(...);
@@ -31,10 +35,26 @@ private func SoundCloseDoor() {
 }
 
 private func CanOpen() {
-	return 1;
+	return !IsResearching();
 }
 
 /* Steuerung */
+
+protected func ContextResearch(object pClonk) {
+	[$TxtResearch$|Image=RSR1|Condition=IsNotResearching]
+	ResearchMenu(pClonk);
+	return 1;
+}
+
+private func IsNotResearching() {
+	return !IsResearching();
+}
+
+protected func ContextCancelResearch(object pClonk) {
+	[$TxtCancelresearch$|Image=RSR2|Condition=IsResearching]
+	PauseAllResearch();
+	return 1;
+}
 
 protected func ContextRemoveSat() {
 	[Sat abmontieren|Image=SATD|Condition=HasSat]
@@ -156,4 +176,165 @@ global func GetBase(object pObject) {
 	var sat = pObject->~HasSat();
 	if(sat) return GetOwner(sat);
 	return NO_OWNER;
+}
+
+/* Forschung */
+
+public func IsResearching() {
+	for(var iNumber, i = 0; iNumber = GetEffect("Research", this, i); i++) {
+		if(GetEffect(0, this, iNumber, 3))
+			return true;
+	}
+}
+
+private func ResearchMenu(object pClonk) {
+  // Hauptmenü erzeugen
+  CreateMenu(RSR1, pClonk, this);
+  // alle gestarteten Forschungen, die noch nicht abgeschlossen wurden, durchgehen
+  for(var iEffectNumber, i = 0; iEffectNumber = GetEffect("Research", this, i); i++) {
+  	EffectVar(0, this, iEffectNumber) = pClonk;
+  	AddMenuItem("$TxtContinuedevelopement$", "SelectResearch", EffectVar(1, this, iEffectNumber), pClonk, 0, iEffectNumber);
+  }
+  
+  AddMenuItem("$TxtResearchs$", "ResearchObjects", RSR3, pClonk, 0, pClonk);
+	AddMenuItem("$TxtResearchs$", "ResearchVehicles", RSR4, pClonk, 0, pClonk);
+	AddMenuItem("$TxtResearchs$", "ResearchStructures", RSR5, pClonk, 0, pClonk);
+}
+
+protected func ResearchObjects(id idImage, object pClonk) { return SelectResearchCategory(idImage, pClonk, C4D_Object); }
+protected func ResearchVehicles(id idImage, object pClonk) { return SelectResearchCategory(idImage, pClonk, C4D_Vehicle); }
+protected func ResearchStructures(id idImage, object pClonk) { return SelectResearchCategory(idImage, pClonk, C4D_Structure); }
+
+private func SelectResearchCategory(id idImage, object pClonk, int iCategory) {
+	// Auswahlmenü erzeugen
+	CreateMenu(idImage, pClonk, this, 0, "$TxtNothingelsetoresearc$");
+	// Alle geladenen Baupläne überprüfen
+	var i, idDef, iPlr = pClonk -> GetOwner();
+	while(idDef = GetDefinition(i++, iCategory)) {
+		// Passende Kategorie und dem Spieler noch nicht bekannt
+		if (GetCategory(0, idDef) & C4D_Knowledge && !GetPlrKnowledge(iPlr, idDef) && idDef -> ~MarsResearch()) {
+			// Keine Forschungsgrundlage nötig oder Forschungsgrundlage vorhanden
+			var aBase = idDef -> ~GetResearchBase(), fHasBase = true;
+			if(aBase) {
+				if(GetType(aBase) != C4V_Array)
+					aBase = [aBase];
+				for(var idBase in aBase)
+					if(!GetPlrKnowledge(iPlr, idBase))
+						fHasBase = false;
+				// Forschungslinks brauchen wir nicht
+			}
+			// Menüeintrag hinzufügen
+			if(fHasBase) 
+				AddMenuItem("$TxtResearchs$", "SelectResearch", idDef, pClonk, 0, pClonk);
+		}
+	}
+}
+
+protected func SelectResearch(id idDef, par) {
+	var iEffectNumber, pClonk;
+	if(GetType(par) == C4V_Int) {
+		iEffectNumber = par;
+		pClonk = EffectVar(0, this, iEffectNumber);
+	}
+	else
+		pClonk = par;
+	if(pClonk -> Contained() != this)
+		return;
+	StartResearch(idDef, pClonk, iEffectNumber);
+}
+
+protected func StartResearch(id idDef, object pClonk, int iEffectNumber) {
+	// Spieler hat diesen Bauplan schon
+	if(GetPlrKnowledge(pClonk -> GetOwner(), idDef)) {
+		Message("$TxtAlreadyDeveloped$", this, GetName(0, idDef));
+		Sound("Error");
+		if(iEffectNumber)
+			RemoveEffect(0, this, iEffectNumber, true);
+		return; 
+  }
+  // Tür muss zu sein
+  if(DoorActive) {
+  	// gleich nochmal versuchen
+  	ScheduleCall(this, "SelectResearch", 10, 0, idDef, pClonk, iEffectNumber);
+  	return;
+  }
+  // Besitzer anpassen
+  SetOwner(pClonk -> GetOwner());
+	// Pause
+	PauseAllResearch();
+  // gibt es bereits einen entsprechenden Effekt?
+  if(iEffectNumber)
+  	ContinueResearch(iEffectNumber);
+  // Effekt erstellen
+  else
+  	AddEffect("Research", this, 100, 15, this, GetID(), idDef, pClonk);
+}
+
+private func PauseAllResearch() {
+  // alle gestarteten Forschungen pausieren
+  for(var iNumber, i = 0; iNumber = GetEffect("Research", this, i); i++) {
+  	PauseResearch(iNumber);
+  }
+  Sound("Lab_Research", 0, 0, 0, 0, -1);
+}
+
+// es ist nicht sichergestellt, dass danach keine Forschung mehr läuft
+// daher keine Änderung der Statuslichter
+private func PauseResearch(int iNumber) {
+	return ChangeEffect(0, this, iNumber, GetEffect(0, this, iNumber, 1), 0);
+}
+
+private func ContinueResearch(int iNumber) {
+	Sound("Lab_Research", 0, 0, 0, 0, +1);
+	return ChangeEffect(0, this, iNumber, GetEffect(0, this, iNumber, 1), 15);
+}
+
+protected func Ejection() {
+	PauseAllResearch();
+	return _inherited(...);
+}
+
+/* EffectVars:
+ * 0: temporär gespeicherter Clonk
+ * 1: ID, die erforscht wird
+ * 2: benötigte Gesamtzeit
+ * 3: bisherige Forschungsschritte
+ * 4: Sound wird abgespielt?
+ */
+
+protected func FxResearchStart(object pTarget, int iEffectNumber, int iTemp, id idDef, object pClonk) {
+	if(iTemp)
+		return;
+	EffectVar(1, pTarget, iEffectNumber) = idDef;
+	var iTime = idDef -> ~GetResearchTime(this, pClonk);
+	if(!iTime)
+		iTime = 100;
+	EffectVar(2, pTarget, iEffectNumber) = iTime;
+	Sound("Lab_Research", 0, 0, 0, 0, +1);
+	EffectVar(4, pTarget, iEffectNumber) = true;
+}
+
+protected func FxResearchTimer(object pTarget, int iEffectNumber, int iEffectTime) {
+	if(!CheckPower(10)) {
+		Sound("Lab_Research", 0, 0, 0, 0, -1);
+		EffectVar(4, pTarget, iEffectNumber) = false;
+		return;
+	}
+	if(!EffectVar(4, pTarget, iEffectNumber)) {
+		Sound("Lab_Research", 0, 0, 0, 0, +1);
+		EffectVar(4, pTarget, iEffectNumber) = true;
+	}
+	var iProgress = ++EffectVar(3, pTarget, iEffectNumber);
+	if(iProgress == EffectVar(2, pTarget, iEffectNumber))
+		return -1;
+}
+
+protected func FxResearchStop(object pTarget, int iEffectNumber, int iReason, bool fTemp) {
+	if(iReason)
+		return;
+	Sound("Lab_Research", 0, 0, 0, 0, -1);
+	Sound("ResearchDone");
+	SetPlrKnowledge(GetOwner(), EffectVar(1, pTarget, iEffectNumber));
+	// Nachricht ausgeben
+	Message("$Txtsdeveloped$", this, GetName(0, EffectVar(1, pTarget, iEffectNumber)));
 }
