7d521173a1980bb54e4c2ee3a5864f522e986dd5
Nachtfalter <f.graier@gmx.de>
2009-12-23 01:20:31 +0000

    * Chaos inda House!

--- ClonkMars.c4f/Chaos.c4s/Script.c
+++ ClonkMars.c4f/Chaos.c4s/Script.c
@@ -3,7 +3,7 @@
 #strict 2
 
 func Initialize() {
-  SetSkyAdjust(RGBa(255,255,255,128),RGB(64,196,255));
+  SetSkyAdjust(RGBa(200,94,51,90),RGB(200,110,55));
   SetGamma(RGB(50,0,0), RGB(140,100,100), RGB(255,220,220));
   return(1);
 }
