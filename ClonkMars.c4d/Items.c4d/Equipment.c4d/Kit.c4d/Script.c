e28874da6c6569d555d08e7c4dc0c650b9f4ba3e
Luchs <Luchs@euirc.eu>
2009-12-28 16:43:15 +0000

    + Mit dem Splitterkit erstellte Splitter haben abhängig von der Landschaft Rotation

--- ClonkMars.c4d/Items.c4d/Equipment.c4d/Kit.c4d/Script.c
+++ ClonkMars.c4d/Items.c4d/Equipment.c4d/Kit.c4d/Script.c
@@ -7,13 +7,67 @@ static const SPCT_MaxUse = 3;
 local Used;
 
 protected func Activate(object pClonk) {
-	var pSite = CreateConstruction(SPTR, 0, 10, pClonk -> GetOwner(), 1, true, true);
+	var act = pClonk -> GetAction();
+	var dir, r = 0, x = 0, y = 10;
+	if(act == "Hangle") {
+		dir = DIR_Top;
+		r = 180;
+		y = 14;
+	}
+	else if(act == "Scale" || act == "ScaleDown") {
+		dir = pClonk -> GetDir();
+		//y = 10;
+		if(dir == DIR_Left) {
+			r = 90;
+			x = 5;
+		}
+		else {
+			r = 270;
+			x = -5;
+		}
+	}
+	else
+		dir = DIR_Bottom;
+	
+
+	
+	var pSite = CreateConstruction(SPTR, x, y, pClonk -> GetOwner(), 100, false, false);	
 	if(pSite) {
+		// Drehung nach Erdoberfläche
+		pSite -> SetR(r);
+		var x_off = [pSite->GetVertex(1), pSite->GetVertex(3)];
+		var y_off = [pSite->GetVertex(1, true), pSite->GetVertex(3, true)];
+		var slope = pSite -> GetSolidOffset(x_off[0], y_off[0], dir) - pSite -> GetSolidOffset(x_off[1], y_off[1], dir);
+		pSite -> SetR(r + slope*2);
+		
+		// debug
+		/*
+		Message("slope: %d|%v,%v", pSite, slope, x_off, y_off);
+		pSite->CreateParticle("PSpark", x_off[0], y_off[0], 0, 0, 50, RGB(255), pSite);
+		pSite->CreateParticle("PSpark", x_off[1], y_off[1], 0, 0, 50, RGB(0, 255), pSite);
+		*/
+		
 		Used++;
 		if(Used == SPCT_MaxUse)
 			RemoveObject();
 	}
 	return 1;
+}
+
+static const DIR_Top = 2;
+static const DIR_Bottom = 3;
+
+global func GetSolidOffset(int x, int y, int dir) {
+	var i;
+	if(dir == DIR_Left)
+		for (i = -15; GBackSolid(x + i, y) && (i < 15); i++);
+	if(dir == DIR_Right)
+		for (i = -15; GBackSolid(x - i, y) && (i < 15); i++);
+	if(dir == DIR_Top)
+		for (i = -15; GBackSolid(x, y + i) && (i < 15); i++);
+	if(dir == DIR_Bottom)
+		for (i = -15; GBackSolid(x, y - i) && (i < 15); i++);
+	return i;
 }
 
 public func MarsResearch() { return true; }
