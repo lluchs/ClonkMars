b1d13519c654b9216fb64cf2c7a144beca95ce15
Luchs <Luchs@euirc.eu>
2010-01-18 23:33:46 +0000

    + RADO-Funktionalität: Zerstört Meteoriten im Umkreis von 250px nach Ausrichtung der Schüssel

--- ClonkMars.c4d/Structures.c4d/Radio.c4d/Script.c
+++ ClonkMars.c4d/Structures.c4d/Radio.c4d/Script.c
@@ -3,9 +3,44 @@
 #strict 2
 #include B_40
 #include DACT //Damagecontrol
+#include PWRC // Energieverbraucher
 
 public func MaxDamage() { return 20; } //Maximaler Schaden
-protected func Initialize(){SetAction("Wait");}
+protected func Initialize(){
+	SetAction("Panel");
+	AddEffect("DestroyMeteorites", this, 10, 5, this);
+}
 public func MarsResearch() {
 	return true;
+}
+
+protected func ControlLeft() {
+	var iPhase = GetPhase() - 1;
+	if(iPhase < 1) {
+		Sound("Error");
+		return;
+	}
+	SetPhase(iPhase);
+	return 1;
+}
+protected func ControlRight() {
+	var iPhase = GetPhase() + 1;
+	if(iPhase > 25) {
+		Sound("Error");
+		return;
+	}
+	SetPhase(iPhase);
+	return 1;
+}
+
+protected func FxDestroyMeteoritesTimer() {
+	var iPhase = GetPhase();
+	var iAngle = [322,321,327,328,331,334,337,339,341,345,352,0,13,24,33,42,50,53,58,60,61,62,63,64,66][iPhase-1];
+
+	//AngleFree(iAngle) + AngleFree(iAngle - 20) + AngleFree(iAngle + 20);
+	
+	for(var pObj in FindObjects(Find_ID(METO), Find_Distance(250), Find_Angle(iAngle + 20, iAngle - 20), Find_PathFree())) {
+		if(CheckPower(20))
+			pObj -> Hit();
+	}
 }
--- ClonkMars.c4d/System.c4g/FindObject.c
+++ ClonkMars.c4d/System.c4g/FindObject.c
@@ -30,3 +30,23 @@ global func Find_ActionTargets(object pTarget) {
 global func IsActionTarget2 (object pObj) {
 	return GetActionTarget(1) == pObj;
 }
+
+global func Find_Angle() {
+	return Find_Func("IsAtAngle", this, ...);
+}
+
+global func IsAtAngle(object pObj, int iAngle1, int iAngle2, int iX, int iY) {
+	iX += pObj -> GetX();
+	iY += pObj -> GetY();
+	
+	var iAngle = Angle(iX, iY, GetX(), GetY());
+	
+	//Log("%v - %v: w1 %d; w2 %d; w %d", pObj, this, iAngle1, iAngle2, iAngle);
+	
+	if(iAngle <= iAngle1 && iAngle >= iAngle2)
+		return 1;
+	
+	// Spezialfall: Winkel 1 kleiner als Winkel 2, z.B. 10; 350
+	if(iAngle1 < iAngle2 && (iAngle <= iAngle1 || iAngle >= iAngle2))
+		return 1;
+}
