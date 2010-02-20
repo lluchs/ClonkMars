f0daf5cf3b17c73e7033de1b522c95858e1f89a5
Nachtfalter <f.graier@gmx.de>
2010-02-20 21:34:09 +0000

    + Crys.c4s

---
+++ ClonkMars.c4f/Crys.c4s/Script.c
@@ -0,0 +1,10 @@
+/*-- Crystal Mountain --*/
+
+#strict
+
+protected func Initialize(){
+  SetSkyAdjust(RGBa(255,255,255,220), RGB(50,30,50));
+  SetGamma(RGB(0,0,50), RGB(100,100,140), RGB(220,220,255));
+  return(1);
+  }
+  
