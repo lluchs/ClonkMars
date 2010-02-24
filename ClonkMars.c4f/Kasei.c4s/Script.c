6c60a3f6bd75271594b98201a098d215abc7299b
Nachtfalter <f.graier@gmx.de>
2010-02-24 19:00:12 +0000

    ! Kasei entbuggt ! Texmap neu sortiert (Shape Problem gefixt)

--- ClonkMars.c4f/Kasei.c4s/Script.c
+++ ClonkMars.c4f/Kasei.c4s/Script.c
@@ -3,7 +3,7 @@
 #strict
 
 func Initialize() {
-  SetSkyAdjust(RGBa(255,255,255,180), RGB(120,80,40));
+  SetSkyAdjust(RGBa(255,255,255,160), RGB(70,30,15));
   SetGamma(RGB(50,0,0), RGB(140,100,100), RGB(255,220,220));
   return(1);
 }
