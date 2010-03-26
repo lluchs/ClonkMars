38a86917610b1a7fd264db98c2ff6c85a3ddbd1d
Nachtfalter <f.graier@gmx.de>
2010-03-26 20:48:40 +0000

    * Chasma aufgemotzt

--- ClonkMars.c4f/Chasma.c4s/Script.c
+++ ClonkMars.c4f/Chasma.c4s/Script.c
@@ -1,8 +1,21 @@
-/*-- Neues Szenario --*/
+/*-- Chasma --*/
 
 #strict
 
 func Initialize() {
-   
+  SetSkyAdjust(RGBa(255,255,255,160), RGB(70,30,15));
+  SetGamma(RGB(50,0,0), RGB(140,100,100), RGB(255,220,220));
   return(1);
 }
+
+protected func InitializePlayer(iPlr){
+  //ersten Clonk alles in die Hand geben
+  CreateContents(CNKT,GetCrew(iPlr,0),2);
+  CreateContents(METL,GetCrew(iPlr,0),2);
+  CreateContents(SATD,GetCrew(iPlr,0),1);
+  CreateContents(PSTC,GetCrew(iPlr,0),2);
+  return(1);
+  }
+
+  //Regelwähler
+  func NotChoosableRules() { return [LNGT,ENRG,CNMT]; }
