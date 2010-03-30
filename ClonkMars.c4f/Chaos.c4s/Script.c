db334aa14a784cda6f93272245bedfad6de616fd
ala <david.oerther@web.de>
2010-03-30 13:51:25 +0000

    Scenario changes (again)

--- ClonkMars.c4f/Chaos.c4s/Script.c
+++ ClonkMars.c4f/Chaos.c4s/Script.c
@@ -5,8 +5,17 @@
 func Initialize() {
   SetSkyAdjust(RGBa(200,94,51,90),RGB(200,110,55));
   SetGamma(RGB(50,0,0), RGB(140,100,100), RGB(255,220,220));
+  
   return(1);
-}
+  }
 
+protected func InitializePlayer(int iPlr) {
+	var pCapsule = CreateObject(CPSL, RandomX(100, LandscapeWidth() - 100), 70, iPlr);
+	for(var pObj in FindObjects(Find_Owner(iPlr), Find_Or(Find_Category(C4D_Object), Find_OCF(OCF_CrewMember)))) {
+		pObj -> Enter(pCapsule);
+	}
+	pCapsule->SetDstPort(0, true);
+}  
+  
   //Regelwähler
-  func NotChoosableRules() { return [ENRG,CNMT]; }
+func NotChoosableRules() { return [ENRG,CNMT]; }
