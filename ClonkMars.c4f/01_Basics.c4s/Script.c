91b2cec4fb255106ec4578d14ff00e22892b703f
Nachtfalter <f.graier@gmx.de>
2010-04-14 00:19:26 +0000

    + Tutorial1

---
+++ ClonkMars.c4f/01_Basics.c4s/Script.c
@@ -0,0 +1,57 @@
+/*-- Basics --*/
+
+#strict
+
+func Initialize() {
+  ScriptGo(1);
+  CreateConstruction(BASE,50,320,0,100,1,0);  //1
+  CreateConstruction(BASE,840,310,0,100,1,0); //2
+  
+  SetSkyAdjust(RGBa(200,94,51,90),RGB(200,110,55));
+  SetGamma(RGB(50,0,0), RGB(140,100,100), RGB(255,220,220));
+  //Hintergrund
+  CreateObject(BG_2,-0,0,-1);
+  
+  return(1);
+}
+
+func Script10()
+{
+  MsgWindow(GetCrew(), "$Txt01$");
+}
+
+func Script12()
+{
+  if(!GetMenu(GetCrew()))
+  {
+    MsgWindow(GetCrew(), "$Txt02$");
+    var Arrow=SetArrowToObj(FindObject2(Find_ID(BASE), Find_InRect(10, 210, 100, 100)));
+  }
+  else return goto(11);
+}
+
+func Script14()
+{
+  //+ Bedingung, wenn in Basis
+  if(!GetMenu(GetCrew()))
+  {
+    MsgWindow(GetCrew(), "$Txt03$");
+    RemoveArrow();
+    
+  }
+  else return goto(13);
+}
+
+
+
+
+global func MsgWindow(pClonk, szText)
+{
+  Sound("Command");
+  pClonk->SetComDir(COMD_Stop);
+  CreateMenu(GetID(pClonk), pClonk, 0, 0, "", 0, 3);
+  AddMenuItem("Portrait:SCNK::ff7777::3", "", NONE, pClonk, 0, 0, "", 5, 0, 0);
+  AddMenuItem(szText, "", NONE, pClonk, 0, 0, "", 512);
+  SetMenuTextProgress(1, pClonk);
+  SetMenuDecoration(MD69, pClonk);
+}
