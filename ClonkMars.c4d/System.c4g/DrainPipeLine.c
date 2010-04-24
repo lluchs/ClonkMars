c2f6c4cc94ca22a27e3d555b194141ad3e6ba0df
Luchs <Luchs@euirc.eu>
2010-04-24 18:23:10 +0000

    * "Abflussrohr" -> "Rohr"

--- ClonkMars.c4d/System.c4g/DrainPipeLine.c
+++ ClonkMars.c4d/System.c4g/DrainPipeLine.c
@@ -1,8 +1,13 @@
-/* PipeHead */
+/* Pipeline */
 
 #strict 2
 
 #appendto DPIP
+
+protected func Initialize() {
+	SetName("$Pipe$", 0, GetID());
+	return _inherited(...);
+}
 
 public func Transfer() {
 	return;
