c65e728b39e56f6019ab4f0ba5bcafd238eb48fd
Luchs <Luchs@euirc.eu>
2010-07-11 09:44:54 +0000

    + Terraforming-Ziel

---
+++ ClonkMars.c4d/Goals.c4d/Terraforming.c4d/Script.c
@@ -0,0 +1,27 @@
+/*-- Terraforming --*/
+
+#strict 2
+#include GOAL
+
+local percent;
+
+public func IsFulfilled() {
+	var iGreen = 0;
+	// alle stehenden Bäume
+	for(var pTree in FindObjects(Find_Category(C4D_StaticBack), Find_Func("IsTree"))) {
+		iGreen += GetDefWidth(pTree -> GetID()) * pTree -> GetCon() / 100;
+	}
+	percent = iGreen / LandscapeWidth();
+	return percent >= GetGoal();
+}
+
+private func GetGoal() {
+	return GameCall("TerraformingGoal") || 100;
+}
+
+protected func Activate(int iPlr) {
+	if(IsFulfilled())
+		return MessageWindow("Huge Success! \\o/", iPlr);
+	else
+		return MessageWindow(Format("Aktueller Stand: %d; zu erreichen: %d", percent, GetGoal()), iPlr);
+}
