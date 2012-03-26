68fdbc69149cd689ab5a3da49ff431ba525482c2
Luchs <Luchs@euirc.eu>
2012-03-26 22:21:26 +0000

    + Cooles Licht fürs Blinklicht

--- ClonkMars.c4d/Items.c4d/Equipment.c4d/Beacon.c4d/Positioned.c4d/Script.c
+++ ClonkMars.c4d/Items.c4d/Equipment.c4d/Beacon.c4d/Positioned.c4d/Script.c
@@ -4,30 +4,37 @@
 #include DACT //Damagecontrol
 
 protected func Initialize() {
-  SetAction("Idletime");
+	SetAction("Idletime");
 }
 
-  
+
 protected func ControlThrow() {
-  if(GetAction()=="Idletime")
-    {
-    SetAction("On");
-    }
-      else {
-        SetAction("Idletime");
-        }
-  }    
-  
+	if(GetAction()=="Idletime") {
+		SetAction("On");
+	}
+	else {
+		SetAction("Idletime");
+	}
+}    
+
 protected func ControlDigDouble(pClonk){
-  RemoveObject(this());
-  CreateContents(BEAC,pClonk,1);
-  Sound("Connect");
-  }  
+	RemoveObject(this());
+	CreateContents(BEAC,pClonk,1);
+	Sound("Connect");
+}
+
+protected func On() {
+	SetPlrViewRange(10);
+	AddLightFlash(150, 0, -GetDefCoreVal("Height", "DefCore") / 2, RGB(200, 200, 255)); // leicht bläuliches Licht
+}
+
+protected func Off() {
+	SetPlrViewRange(0);
+}
 
 public func MaxDamage() { return 4; } //Maximaler Schaden
 
-protected func Hit()
-{
-  Sound("RockHit*");
+protected func Hit() {
+	Sound("RockHit*");
 	return(1);
 }
