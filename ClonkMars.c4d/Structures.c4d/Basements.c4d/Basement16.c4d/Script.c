4f2faf3c070f542bc47db147f33be076a0279c96
Luchs <Luchs@euirc.eu>
2012-04-12 01:14:02 +0000

    ! Bessere Splitterfundamentplatzierung

--- ClonkMars.c4d/Structures.c4d/Basements.c4d/Basement16.c4d/Script.c
+++ ClonkMars.c4d/Structures.c4d/Basements.c4d/Basement16.c4d/Script.c
@@ -1,6 +1,6 @@
 /*-- Fundament --*/
 
-#strict
+#strict 2
 
 /* Alle Fundamente leiten ihre Grundfunktionalität von BAS7 ab */
 
@@ -8,3 +8,19 @@
 
 private func BasementID() { return(B_16); }
 private func BasementWidth() { return(16); }
+
+/* Verschiebt die SolidMask bei Rotation */
+public func SetR(int r) {
+	var ID = GetID();
+	if (r != 0 && ID == BasementID()) {
+		var wdt = GetDefWidth(ID), hgt = GetDefHeight(ID);
+		var ssgn = -1, csgn = 1;
+		if (Abs(r) > 90) {
+			ssgn *= -1;
+			csgn *= -1;
+		}
+		SetSolidMask(0, 0, wdt, hgt, ssgn*Sin(r, hgt / 2), csgn*Cos(r, hgt / 2));
+	}
+	return inherited(r, ...);
+}
+
