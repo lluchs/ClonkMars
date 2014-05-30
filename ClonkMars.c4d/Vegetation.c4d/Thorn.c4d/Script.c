1b62c65e28b061a584bb373292d0596ce50ab673
Nachtfalter <f.graier@gmx.de>
2014-05-30 14:24:31 +0200

    Add Thorn Object

---
+++ ClonkMars.c4d/Vegetation.c4d/Thorn.c4d/Script.c
@@ -0,0 +1,50 @@
+/*-- Stalaktit --*/
+
+#strict
+
+local phase;
+
+func Initialize() {
+  SetAction("Hanging");
+  SetPhase(phase=Random(4));
+  
+    // Drehung nach Erdoberfläche
+  var x_off = 18 * GetCon() / 100;
+  var y_off = 15 * GetCon() / 100;
+  var slope = GetSolidOffset(-x_off, y_off) - GetSolidOffset(x_off, y_off);
+  SetR(slope);
+}
+
+protected func Damage()
+{
+  // Wenn der Baum schon zu viel Schaden genommen hat...
+  if (GetDamage() > 10)
+    {
+    Destroy();
+	}
+  return(1);
+}
+
+func Destroy() {
+  var obj;
+  if(GetAction()ne"Falling") return(1);
+  Sound("RockBreak*");
+  Sound("Blast2",0,0,50);
+  // links oben
+  ObjectSetAction(obj=CreateObject(_STP,-4,-4,-1),"Exist");
+  SetPhase(phase*2,obj);
+  SetRDir(Random(20)-10,obj);
+  SetSpeed(Random(30)-15,-Random(35)+10,obj);
+  // rechts oben
+  ObjectSetAction(obj=CreateObject(_STP,3,-4,-1),"Exist");
+  SetPhase(phase*2+1,obj);
+  SetRDir(Random(20)-10,obj);
+  SetSpeed(Random(30)-15,-Random(35)+10,obj);
+  // unten
+  ObjectSetAction(obj=CreateObject(_STP,0,12,-1),"LameExist");
+  SetPhase(phase,obj);
+  SetRDir(Random(20)-10,obj);
+  SetSpeed(Random(10)-15,-Random(35)+10,obj);
+	
+  RemoveObject();
+}
